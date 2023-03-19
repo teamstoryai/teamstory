05c7400735ef944d6e1d72ba11c5796ddf5fbd9f
Tim Su <timsu@users.noreply.github.com>
2023-03-18 21:42:09 -0700

    Yarn install. Get full server running.

--- lib/teamstory_web/views/projects_view.ex
+++ lib/teamstory_web/views/projects_view.ex
@@ -1,4 +1,3 @@
-
 defmodule TeamstoryWeb.ProjectsView do
   use TeamstoryWeb, :view
 
@@ -6,7 +5,6 @@ defmodule TeamstoryWeb.ProjectsView do
     %{
       id: Teamstory.Utils.no_dash(project.uuid),
       name: project.name,
-      shortcode: project.shortcode,
       archived_at: project.archived_at
     }
   end
@@ -14,21 +12,20 @@ defmodule TeamstoryWeb.ProjectsView do
   def render("list.json", %{projects: projects, user: user}) do
     %{
       user: TeamstoryWeb.AuthView.render_user(user),
-      projects: projects |> Enum.map(&render_project(&1, user)),
+      projects: projects |> Enum.map(&render_project(&1, user))
     }
   end
 
   def render("get.json", %{project: project, user: user, members: members}) do
     %{
       project: render_project(project, user),
-      members: members,
+      members: members
     }
   end
 
   def render("get.json", %{project: project, user: user}) do
     %{
-      project: render_project(project, user),
+      project: render_project(project, user)
     }
   end
-
 end
