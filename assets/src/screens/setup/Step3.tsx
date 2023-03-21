6fd116986faccd08753b9005c818d94db53494a6
Tim Su <timsu@users.noreply.github.com>
2023-03-21 12:23:35 -0700

    Add what's important onboarding field

--- assets/src/screens/setup/Step3.tsx
+++ assets/src/screens/setup/Step3.tsx
@@ -9,12 +9,59 @@ import { tokenStore } from '@/stores/tokenStore'
 import { logger, toTitleCase } from '@/utils'
 import ErrorMessage from '@/components/core/ErrorMessage'
 import { OAuthToken } from '@/models'
+import { API } from '@/api'
+import Input from '@/components/core/Input'
+import Submit from '@/components/core/Submit'
 
 export const Step3 = ({ setStep }: { setStep: StateUpdater<number> }) => {
+  const [whatsImportant, setWhatsImportant] = useState('')
+  const [saveState, setSaveState] = useState<'idle' | 'saving' | 'saved' | 'error'>('idle')
+
+  useEffect(() => {
+    API.getUserData('setup').then((data) => {
+      setWhatsImportant(data.whatsImportant || '')
+    })
+  }, [])
+
+  const onSubmit = async (e: Event) => {
+    e.preventDefault()
+    setSaveState('saving')
+    API.setUserData('setup', { whatsImportant })
+      .then((e) => setSaveState('saved'))
+      .catch((e) => {
+        logger.error(e)
+        setSaveState('error')
+      })
+  }
+
+  const saveLabel =
+    saveState === 'saving'
+      ? 'Saving...'
+      : saveState == 'saved'
+      ? 'Saved'
+      : saveState == 'error'
+      ? 'Error Saving'
+      : 'Save'
+
   return (
     <div class="mb-12">
       <h1 class="text-lg my-2">3. Onboarding</h1>
-      <p class="text-gray-700">Awesome! We will contact you when your dashboard is ready.</p>
+
+      <p class="text-gray-700">
+        Awesome! We will contact you when your dashboard is ready. If you have some time, please
+        tell us what you're hoping for in a team dashboard:
+      </p>
+
+      <form onSubmit={onSubmit} class="mt-2 max-w-lg">
+        <textarea
+          placeholder="What's important to you?"
+          value={whatsImportant}
+          onChange={(e) => setWhatsImportant((e.target as HTMLTextAreaElement).value)}
+          class="w-full border border-gray-300 rounded-md p-2 my-2"
+        />
+
+        <Submit disabled={saveState == 'saving'} label={saveLabel} />
+      </form>
 
       <p class="text-gray-700 my-2">
         If it's been a few days, please reach out to{' '}
--- lib/teamstory_web/controllers/user_data_controller.ex
+++ lib/teamstory_web/controllers/user_data_controller.ex
@@ -7,40 +7,50 @@ defmodule TeamstoryWeb.UserDataController do
   action_fallback TeamstoryWeb.FallbackController
 
   # GET "/users/data"
-  def get_user_data(conn, %{ "key" => key } = params) do
+  def get_user_data(conn, %{"key" => key} = params) do
     with user when is_map(user) <- Guardian.Plug.current_resource(conn) do
-
       {_, user_data} = user_data_helper(user, params, key)
-      json conn, %{ data: user_data && user_data.value }
+      IO.inspect(user_data)
+      json(conn, %{data: user_data && user_data.value})
     end
   end
 
   # POST "/users/data"
-  def set_user_data(conn, %{ "key" => key, "data" => data } = params) do
+  def set_user_data(conn, %{"key" => key, "data" => data} = params) do
     with user when is_map(user) <- Guardian.Plug.current_resource(conn) do
-
       {project, user_data} = user_data_helper(user, params, key)
+      IO.inspect(user_data)
 
-      if !user_data do
-        Users.create_user_data(%{ user_id: user.id, project_id: project && project.id, key: key, value: data })
-      else
-        if data do
-          Users.update_user_data(user_data, %{ value: data })
+      result =
+        if !user_data do
+          Users.create_user_data(%{
+            user_id: user.id,
+            project_id: project && project.id,
+            key: key,
+            value: data
+          })
         else
-          Users.delete_user_data(user_data)
+          if data do
+            Users.update_user_data(user_data, %{value: data})
+          else
+            Users.delete_user_data(user_data)
+          end
         end
+
+      with {:ok, _} <- result do
+        :ok
       end
-      :ok
     end
   end
 
   defp user_data_helper(user, params, key) do
-    project = if params["project"] do
-      {:ok, project} = Projects.project_by_uuid(user.id, params["project"])
-      project
-    end
+    project =
+      if params["project"] do
+        {:ok, project} = Projects.project_by_uuid(user.id, params["project"])
+        project
+      end
+
     user_data = Users.get_user_data(user, project, key)
     {project, user_data}
   end
-
 end
